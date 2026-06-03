e274df8c9564a696009f88304aafc05247ce572e
lkocis <168871650+lkocis@users.noreply.github.com>
2026-06-03 09:53:23 +0200

    Added code for opening the camera and filming.

---
+++ config.py
@@ -0,0 +1,12 @@
+CAMERA_INDEX = 0          # 0 je obično ugrađena web kamera
+FRAME_WIDTH = 1280        # HD rezolucija za bolji preview
+FRAME_HEIGHT = 720
+
+# UX i Tekstualne poruke
+FONT_SCALE = 0.8
+FONT_COLOR = (255, 255, 255)  # Bijela boja
+BG_TEXT_COLOR = (0, 0, 0)     # Crna pozadina za bolju čitljivost koda
+
+# Putanje do mapa
+EFFECTS_DIR = "effects"
+PHOTOS_DIR = "photos"
